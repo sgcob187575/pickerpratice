70b563f1333d9c77f374543416ce7b7a4e59a0e7
YuHao <sgcob187575@gmail.com>
2020-04-19 13:42:15 +0800

    Initial Commit

---
+++ pickerpratice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pickerpratice
+//
+//  Created by 陳昱豪 on 2020/4/19.
+//  Copyright © 2020 Chen_Yu_Hao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
